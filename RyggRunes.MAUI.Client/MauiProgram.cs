337087f105572f648ae71939a8004ee6204655a3
jlind0 <lind@yahoo.com>
2023-08-21 12:33:01 -0500

    support keychain

--- RyggRunes.MAUI.Client/MauiProgram.cs
+++ RyggRunes.MAUI.Client/MauiProgram.cs
@@ -40,6 +40,7 @@ namespace RyggRunes.MAUI.Client
                 .WithRedirectUri(builder.Configuration["AzureAD:RedirectURI"]) // needed only for the system browser
 #elif IOS
                 .WithRedirectUri(builder.Configuration["AzureAD:iOSRedirectURI"])
+                .WithIosKeychainSecurityGroup(builder.Configuration["AzureAD:iOSKeyChainGroup"])
 #elif MACCATALYST
                 .WithRedirectUri(builder.Configuration["AzureAD:iOSRedirectURI"])
 #endif                
