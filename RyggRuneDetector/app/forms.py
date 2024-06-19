482ba86944fef513d73c1814d5e0c52018a0d72f
Matthew Cengia <mattcen@mattcen.com>
2024-06-19 17:05:25 +1000

    format and lint
    ruff check --extend-select I --fix; ruff format

--- RyggRuneDetector/app/forms.py
+++ RyggRuneDetector/app/forms.py
@@ -6,18 +6,21 @@ from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 from django.utils.translation import gettext_lazy as _
 
+
 class ImageUploadForm(forms.Form):
     image = forms.ImageField()
 
+
 class BootstrapAuthenticationForm(AuthenticationForm):
     """Authentication form which uses boostrap CSS."""
-    username = forms.CharField(max_length=254,
-                               widget=forms.TextInput({
-                                   'class': 'form-control',
-                                   'placeholder': 'User name'}))
-    password = forms.CharField(label=_("Password"),
-                               widget=forms.PasswordInput({
-                                   'class': 'form-control',
-                                   'placeholder':'Password'}))
- 
- 
+
+    username = forms.CharField(
+        max_length=254,
+        widget=forms.TextInput({"class": "form-control", "placeholder": "User name"}),
+    )
+    password = forms.CharField(
+        label=_("Password"),
+        widget=forms.PasswordInput(
+            {"class": "form-control", "placeholder": "Password"}
+        ),
+    )
